f2f7357fb1f3150571bb90336ce5cb61c436086b
tolstovrob <tolstovrob@gmail.com>
2024-12-15 12:17:27 +0400

    feat: add discrete looping system template

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,19 @@
 #include "models/Student.h"
 #include "models/Money.h"
 
+#include "services/loop.cpp"
+
 int main() {
-  EnrollRequest enrollRequest(1, 1, "John", "Doe");
-  GroupClass group(2, 1, {{enrollRequest.getId(), true}, {3, false}}, Money(1000, 0));
-  IndividualClass individual(2, 1, {enrollRequest.getId(), true}, Money(1000, 0));
-  Course course(3, "Introduction to Programming", A1, INTENSIVE);
-  Student student(4, "Jane", "Doe");
+  std::vector<EnrollRequest> enrollRequests;
+  std::vector<Student> students;
+  std::vector<Course> courses;
+  std::vector<GroupClass> groupClasses;
+  std::vector<IndividualClass> individualClasses;
+
+  while (true) {
+    loop();
+    std::cin.get();
+  }
 
   std::cout << "All models has been loaded successfully!" << std::endl;
   return 0;  
---
+++ src/services/loop.cpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef LOOP_CPP
+#define LOOP_CPP
+
+#include <iostream>
+
+bool loop() {
+  std::cout << "2 weeks have passed" << std::endl;
+  return true;
+};
+
+#endif // LOOP_H
