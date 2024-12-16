cf943ab397173991ca6ebba19b0d64a2b4c4acd2
tolstovrob <tolstovrob@gmail.com>
2024-12-16 20:42:20 +0400

    refactor: update all class vectors with std::shared_ptr

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <memory>
 
 #include "models/EnrollRequest/EnrollRequest.h"
 #include "models/GroupClass/GroupClass.h"
@@ -12,17 +13,25 @@
 #include "services/loop.cpp"
 
 int main() {
-  std::vector<EnrollRequest> enrollRequests;
-  std::vector<Student> students;
-  std::vector<Course> courses;
-  std::vector<GroupClass> groupClasses;
-  std::vector<IndividualClass> individualClasses;
+  setlocale(LC_ALL, "ru_RUSSIAN.UTF-8");
+  std::vector<std::shared_ptr<EnrollRequest>> enrollRequests;
+  std::vector<std::shared_ptr<Student>> students;
+  std::vector<std::shared_ptr<Course>> courses;
+  std::vector<std::shared_ptr<GroupClass>> groupClasses;
+  std::vector<std::shared_ptr<IndividualClass>> individualClasses;
 
+  std::cout << "Добро пожаловать в систему контроля курсов иностранных языков" << std::endl;
+  std::cout << "Нажмите любую клавишу для продолжения..." << std::endl;
+  std::cin.get();
+
+  unsigned long long iteration = 0;
   while (true) {
-    loop();
+    if (!loop(iteration, enrollRequests, students, courses, groupClasses, individualClasses)) {
+      break;
+    };
     std::cin.get();
   }
 
-  std::cout << "All models has been loaded successfully!" << std::endl;
+  std::cout << "Моделирование остановлено!" << std::endl;
   return 0;  
 }
--- src/services/loop.cpp
+++ src/services/loop.cpp
@@ -3,8 +3,47 @@
 #define LOOP_CPP
 
 #include <iostream>
+#include <vector>
 
-bool loop() {
+#include "models/EnrollRequest/EnrollRequest.h"
+#include "models/GroupClass/GroupClass.h"
+#include "models/IndividualClass/IndividualClass.h"
+#include "models/Course/Course.h"
+#include "models/Student/Student.h"
+#include "models/Money/Money.h"
+
+bool loop(unsigned long long iteration,
+          std::vector<std::shared_ptr<EnrollRequest>> &enrollRequests,
+          std::vector<std::shared_ptr<Student>> &students,
+          std::vector<std::shared_ptr<Course>> &courses,
+          std::vector<std::shared_ptr<GroupClass>> &groupClasses,
+          std::vector<std::shared_ptr<IndividualClass>> &individualClasses) {
+
+  bool error = false;
+
+  std::cout << "Итерация: " << iteration << std::endl;
+  if (iteration == 0) {
+    std::cout << "Загрузите начальные состояния..." << std::endl;
+  } else {
+    std::cout << "Обновляем состояния..." << std::endl;
+  }
+
+  unsigned long long enrollRequestCount;
+  
+  try {
+    std::cout << "Количество заявок на очередное вступление: ";
+    std::cin >> enrollRequestCount;
+  } catch (std::exception &e) {
+    error = true;
+    return error;
+  }
+  
+  for(int i = 0; i < enrollRequestCount; ++i) {
+    // EnrollRequest enrollRequest;
+    
+    // enrollRequests.push_back(enrollRequest);
+  }
+  
   std::cout << "2 weeks have passed" << std::endl;
   return true;
 };
