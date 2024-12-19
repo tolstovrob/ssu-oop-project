369f211d60922b3d2eccd489393b7159438f1440
tolstovrob <tolstovrob@gmail.com>
2024-12-19 20:24:54 +0400

    refactor: remove unused util

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -6,18 +6,6 @@
 #include <vector>
 #include <memory>
 
-#include "models/Identifiable/Identifiable.h"
-
-unsigned long long getFreeId(const std::vector<std::shared_ptr<Identifiable>>& identifiableEntities) {
-  unsigned long long maxId = 0;
-
-  for (const auto& identifiableEntitiy : identifiableEntities) {
-    maxId = std::max(maxId, identifiableEntitiy->getId());
-  }
-
-  return maxId + 1;
-}
-
 unsigned long long getFreeId(const std::vector<unsigned long long>& ids) {
   unsigned long long maxId = 0;
 
