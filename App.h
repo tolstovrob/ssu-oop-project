6f2898459319d6917d55bd88063f1baaff4fda6b
tolst <tolst@TOLSTOVROB-PC>
2024-12-22 06:05:59 +0400

    Добавьте файлы проекта.

---
+++ App.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace CourseManagement {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ App
+	/// </summary>
+	public ref class App : public System::Windows::Forms::Form
+	{
+	public:
+		App(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~App()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// App
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1252, 729);
+			this->Name = L"App";
+			this->Text = L"App";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
